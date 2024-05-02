44894de2cfc81c0b64282a9e9b90ce85f036426d
goztrk <me@gokhan.org.tr>
2024-05-02 08:12:29 +0300

    feat: change copilot accept to `tab`

--- lua/goztrk/lazy/copilot.lua
+++ lua/goztrk/lazy/copilot.lua
@@ -1,36 +1,38 @@
 return {
-  {
-    'zbirenbaum/copilot.lua',
-    dependencies = {
-      { 'hrsh7th/nvim-cmp' },
-    },
-    enabled = true,
-    cmd = 'Copilot',
-    event = 'InsertEnter',
-    build = ':Copilot auth',
-    config = function()
-      require('copilot').setup({
-        panel = {
-          enabled = true,
-          auto_refresh = true,
-        },
-        suggestion = {
-          enabled = true,
-          auto_trigger = true,
-          accept = false,
-        },
-      })
+	{
+		"zbirenbaum/copilot.lua",
+		dependencies = {
+			{ "hrsh7th/nvim-cmp" },
+		},
+		enabled = true,
+		cmd = "Copilot",
+		event = "InsertEnter",
+		build = ":Copilot auth",
+		config = function()
+			require("copilot").setup({
+				panel = {
+					enabled = true,
+					auto_refresh = true,
+				},
+				suggestion = {
+					enabled = true,
+					auto_trigger = true,
+					keymap = {
+						accept = "<Tab>",
+					},
+				},
+			})
 
-      local cmp_status_ok, cmp = pcall(require, 'cmp')
-      if cmp_status_ok then
-        cmp.event:on('menu_opened', function()
-          vim.b.copilot_suggestion_hidden = true
-        end)
+			local cmp_status_ok, cmp = pcall(require, "cmp")
+			if cmp_status_ok then
+				cmp.event:on("menu_opened", function()
+					vim.b.copilot_suggestion_hidden = true
+				end)
 
-        cmp.event:on('menu_closed', function()
-          vim.b.copilot_suggestion_hidden = false
-        end)
-      end
-    end,
-  },
+				cmp.event:on("menu_closed", function()
+					vim.b.copilot_suggestion_hidden = false
+				end)
+			end
+		end,
+	},
 }
