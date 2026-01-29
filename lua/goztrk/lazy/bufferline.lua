6fe1d3705b6feecd3f343a115f7a103cfe1edd63
gwainor <me@gokhan.tr>
2026-01-30 00:26:59 +0300

    feat: update theme options
    - Make bufferline to use tokyonight.
    - Comment out other theme configs because they were getting in the way.
    - Update telescope theme, make it borderless

--- lua/goztrk/lazy/bufferline.lua
+++ lua/goztrk/lazy/bufferline.lua
@@ -4,15 +4,12 @@ return {
 		event = "ColorScheme",
 		version = "*",
 		dependencies = "nvim-tree/nvim-web-devicons",
-    enabled = function()
-      return not vim.g.started_by_firenvim
-    end,
+		enabled = function()
+			return not vim.g.started_by_firenvim
+		end,
 		config = function()
 			require("bufferline").setup({
-				highlights = require("rose-pine.plugins.bufferline"),
-        options = {
-          persist_buffer_sort = false,
-        },
+				options = { persist_buffer_sort = false },
 			})
 		end,
 	},
--- lua/goztrk/lazy/theme.lua
+++ lua/goztrk/lazy/theme.lua
@@ -1,118 +1,148 @@
 return {
-  {
-    "rose-pine/neovim",
-    priority = 100,
-    name = "rose-pine",
-    config = function()
-      require("rose-pine").setup({
-        --- @usage 'auto'|'main'|'moon'|'dawn'
-        variant = "main",
-        --- @usage 'main'|'moon'|'dawn'
-        dark_variant = "main",
-        bold_vert_split = false,
-        dim_inactive_windows = false,
-        dim_nc_background = false,
-        disable_background = true,
-        disable_float_background = false,
-        disable_italics = false,
-
-        enable = {
-          terminal = true,
-          -- Improve compatibility for previous versions of Neovim
-          legacy_highlights = true,
-          -- Handle deprecated options automatically
-          migrations = true,
-        },
-
-        styles = {
-          bold = true,
-          italic = true,
-          transparency = true,
-        },
-
-        --- @usage string hex value or named color from rosepinetheme.com/palette
-        groups = {
-          border = "muted",
-          link = "iris",
-          panel = "surface",
-
-          error = "love",
-          hint = "iris",
-          info = "foam",
-          note = "pine",
-          todo = "rose",
-          warn = "gold",
-
-          git_add = "foam",
-          git_change = "rose",
-          git_delete = "love",
-          git_dirty = "rose",
-          git_ignore = "muted",
-          git_merge = "iris",
-          git_rename = "pine",
-          git_stage = "iris",
-          git_text = "rose",
-          git_untracked = "subtle",
-
-          h1 = "iris",
-          h2 = "foam",
-          h3 = "rose",
-          h4 = "gold",
-          h5 = "pine",
-          h6 = "foam",
-        },
-
-        -- https://github.com/rose-pine/neovim/wiki/Recipes
-        highlight_groups = {
-          StatusLine = { fg = "love", bg = "love", blend = 10 },
-          StatusLineNC = { fg = "subtle", bg = "surface" },
-
-          TelescopeBorder = { fg = "highlight_high", bg = "base" },
-          TelescopeNormal = { bg = "base" },
-          TelescopePromptNormal = { bg = "base" },
-          TelescopeResultsNormal = { fg = "subtle", bg = "base" },
-          TelescopeSelection = { fg = "text", bg = "base" },
-          TelescopeSelectionCaret = { fg = "rose", bg = "rose" },
-        },
-      })
-      vim.cmd([[colorscheme rose-pine]])
-    end,
-  },
-  {
-    "catppuccin/nvim",
-    name = "catppuccin",
-    priority = 1000,
-    config = function()
-      require("catppuccin").setup({
-        flavor = "mocha",
-        transparent_background = true,
-        show_end_of_buffer = true,
-        custom_highlights = function(colors)
-          return {
-            StatusLine = { fg = colors.base0, bg = colors.surface0 },
-            StatusLineNC = { fg = colors.base0, bg = colors.base4 },
-          }
-        end,
-        integrations = {
-          cmp = true,
-          flash = true,
-          fidget = true,
-        },
-      })
-      -- vim.cmd([[colorscheme catppuccin]])
-    end,
-  },
-  {
-    "folke/tokyonight.nvim",
-    lazy = false,
-    priority = 1000,
-    opts = {},
-    config = function()
-      require("tokyonight").setup({
-        style = 'moon',
-        transparent = true,
-      })
-      vim.cmd([[colorscheme tokyonight]])
-    end,
-  },
+	-- {
+	--   "rose-pine/neovim",
+	--   priority = 100,
+	--   name = "rose-pine",
+	--   config = function()
+	--     require("rose-pine").setup({
+	--       --- @usage 'auto'|'main'|'moon'|'dawn'
+	--       variant = "main",
+	--       --- @usage 'main'|'moon'|'dawn'
+	--       dark_variant = "main",
+	--       bold_vert_split = false,
+	--       dim_inactive_windows = false,
+	--       dim_nc_background = false,
+	--       disable_background = true,
+	--       disable_float_background = false,
+	--       disable_italics = false,
+	--
+	--       enable = {
+	--         terminal = true,
+	--         -- Improve compatibility for previous versions of Neovim
+	--         legacy_highlights = true,
+	--         -- Handle deprecated options automatically
+	--         migrations = true,
+	--       },
+	--
+	--       styles = {
+	--         bold = true,
+	--         italic = true,
+	--         transparency = true,
+	--       },
+	--
+	--       --- @usage string hex value or named color from rosepinetheme.com/palette
+	--       groups = {
+	--         border = "muted",
+	--         link = "iris",
+	--         panel = "surface",
+	--
+	--         error = "love",
+	--         hint = "iris",
+	--         info = "foam",
+	--         note = "pine",
+	--         todo = "rose",
+	--         warn = "gold",
+	--
+	--         git_add = "foam",
+	--         git_change = "rose",
+	--         git_delete = "love",
+	--         git_dirty = "rose",
+	--         git_ignore = "muted",
+	--         git_merge = "iris",
+	--         git_rename = "pine",
+	--         git_stage = "iris",
+	--         git_text = "rose",
+	--         git_untracked = "subtle",
+	--
+	--         h1 = "iris",
+	--         h2 = "foam",
+	--         h3 = "rose",
+	--         h4 = "gold",
+	--         h5 = "pine",
+	--         h6 = "foam",
+	--       },
+	--
+	--       -- https://github.com/rose-pine/neovim/wiki/Recipes
+	--       highlight_groups = {
+	--         StatusLine = { fg = "love", bg = "love", blend = 10 },
+	--         StatusLineNC = { fg = "subtle", bg = "surface" },
+	--
+	--         TelescopeBorder = { fg = "highlight_high", bg = "base" },
+	--         TelescopeNormal = { bg = "base" },
+	--         TelescopePromptNormal = { bg = "base" },
+	--         TelescopeResultsNormal = { fg = "subtle", bg = "base" },
+	--         TelescopeSelection = { fg = "text", bg = "base" },
+	--         TelescopeSelectionCaret = { fg = "rose", bg = "rose" },
+	--       },
+	--     })
+	--     vim.cmd([[colorscheme rose-pine]])
+	--   end,
+	-- },
+	-- {
+	--   "catppuccin/nvim",
+	--   name = "catppuccin",
+	--   priority = 1000,
+	--   config = function()
+	--     require("catppuccin").setup({
+	--       flavor = "mocha",
+	--       transparent_background = true,
+	--       show_end_of_buffer = true,
+	--       custom_highlights = function(colors)
+	--         return {
+	--           StatusLine = { fg = colors.base0, bg = colors.surface0 },
+	--           StatusLineNC = { fg = colors.base0, bg = colors.base4 },
+	--         }
+	--       end,
+	--       integrations = {
+	--         cmp = true,
+	--         flash = true,
+	--         fidget = true,
+	--       },
+	--     })
+	--     -- vim.cmd([[colorscheme catppuccin]])
+	--   end,
+	-- },
+	{
+		"folke/tokyonight.nvim",
+		lazy = false,
+		priority = 1000,
+		opts = {},
+		config = function()
+			require("tokyonight").setup({
+				style = "moon",
+				transparent = true,
+				on_highlights = function(hl, c)
+					local prompt = "#2d3149"
+					hl.TelescopeNormal = {
+						bg = c.bg_dark,
+						fg = c.fg_dark,
+					}
+					hl.TelescopeBorder = {
+						bg = c.bg_dark,
+						fg = c.bg_dark,
+					}
+					hl.TelescopePromptNormal = {
+						bg = prompt,
+					}
+					hl.TelescopePromptBorder = {
+						bg = prompt,
+						fg = prompt,
+					}
+					hl.TelescopePromptTitle = {
+						bg = prompt,
+						fg = prompt,
+					}
+					hl.TelescopePreviewTitle = {
+						bg = c.bg_dark,
+						fg = c.bg_dark,
+					}
+					hl.TelescopeResultsTitle = {
+						bg = c.bg_dark,
+						fg = c.bg_dark,
+					}
+				end,
+			})
+			vim.cmd([[colorscheme tokyonight]])
+		end,
+	},
 }
