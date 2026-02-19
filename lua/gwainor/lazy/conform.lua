f26257cff29734506f7bead235c4c51800656024
gwainor <me@gokhan.tr>
2026-02-20 00:00:17 +0300

    fix: conform ts/js formatter configs

--- lua/gwainor/lazy/conform.lua
+++ lua/gwainor/lazy/conform.lua
@@ -17,16 +17,34 @@ return {
         }),
         formatters = {
           prettier = { require_cwd = true },
-          biome = { require_cwd = true },
+          biome = {
+            require_cwd = true,
+            cwd = require("conform.util").root_file({ "biome.json" })
+          },
+          ["biome-check"] = {
+            require_cwd = true,
+            cwd = require("conform.util").root_file({ "biome.json" })
+          },
+          eslint_d = {
+            require_cwd = true,
+            cwd = require("conform.util").root_file({
+              "eslint.config.js",
+              "eslint.config.mjs",
+              "eslint.config.cjs",
+              "eslint.config.ts",
+              "eslint.config.mts",
+              "eslint.config.cts",
+            }),
+          },
           ruff = { require_cwd = true },
         },
         formatters_by_ft = {
           lua = { "stylua" },
           python = { "ruff_fix", "ruff_format", "ruff_organize_imports" },
-          typescript = { "biome-check", "prettierd", stop_after_first = true },
-          typescriptreact = { "biome-check", "prettierd", stop_after_first = true },
-          javascript = { "biome", "prettierd", stop_after_first = true },
-          javascriptreact = { "biome-check", "prettierd", stop_after_first = true },
+          typescript = { "eslint_d", "biome-check", "prettierd", stop_after_first = true },
+          typescriptreact = { "eslint_d", "biome-check", "prettierd", stop_after_first = true },
+          javascript = { "eslint_d", "biome-check", "prettierd", stop_after_first = true },
+          javascriptreact = { "eslint_d", "biome-check", "prettierd", stop_after_first = true },
           html = { "prettierd" },
           htmldjango = { "prettierd" },
           css = { "prettierd" },
